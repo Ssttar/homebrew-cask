eb7f73c9561314eaaca1aa85f98f6a3d221c79ab
Klaus Hipp <khipp@users.noreply.github.com>
2024-07-30 12:28:21 +0200

    gpgfrontend 2.1.4

--- Casks/g/gpgfrontend.rb
+++ Casks/g/gpgfrontend.rb
@@ -1,16 +1,30 @@
 cask "gpgfrontend" do
-  version "2.1.3"
+  version "2.1.4"
 
-  on_big_sur :or_older do
-    sha256 "a7fa8ded6a28c31a14552b8114371c7a089aa76dc065825e3fedf1139240aa9e"
-
-    url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-11.dmg",
-        verified: "github.com/saturneric/GpgFrontend/"
-  end
-  on_monterey :or_newer do
-    sha256 "e3ef0060b1793cc1731edf96e2223aa0a35ff447aa1d590c90a81db23b14fe59"
+  on_monterey :or_older do
+    sha256 "1618530c448faa8b41a1dabb30b6c10edb6132e9ab52a24729b25fe33e457ded"
 
     url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-12.dmg",
+        verified: "github.com/saturneric/GpgFrontend/"
+
+    caveats do
+      requires_rosetta
+    end
+  end
+  on_ventura do
+    sha256 "f4496007ed067f20503be73fe69d40ae03601c2c3b37c5b8809f82f96d979527"
+
+    url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-13.dmg",
+        verified: "github.com/saturneric/GpgFrontend/"
+
+    caveats do
+      requires_rosetta
+    end
+  end
+  on_sonoma :or_newer do
+    sha256 "a93c8edd12ccd557f3702abf8c12fee877a3574fd594f3501ace68c9db1d4e29"
+
+    url "https://github.com/saturneric/GpgFrontend/releases/download/v#{version}/GpgFrontend-#{version}-macos-14.dmg",
         verified: "github.com/saturneric/GpgFrontend/"
   end
 
@@ -18,7 +32,7 @@ cask "gpgfrontend" do
   desc "OpenPGP/GnuPG crypto, sign and key management tool"
   homepage "https://gpgfrontend.bktus.com/"
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :monterey"
   depends_on formula: "gnupg"
 
   app "GpgFrontend.app"
@@ -32,8 +46,4 @@ cask "gpgfrontend" do
     "~/Library/Preferences/pub.gpgfrontend.gpgfrontend.plist",
     "~/Library/Saved Application State/pub.gpgfrontend.gpgfrontend.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
